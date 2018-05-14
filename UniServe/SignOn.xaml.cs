bafd3dde5d601b25f17a3bf2eff04e91f72ddcde
USER <USER@DESKTOP-ROCI7VV>
2018-05-14 21:40:59 +0530

    Add project files.

---
+++ UniServe/SignOn.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFTabTip;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+            //Bind the virtual keybord to all textBox and PasswordBox
+            TabTipAutomation.BindTo<TextBox>();
+            TabTipAutomation.BindTo<PasswordBox>();
+            // Ignore the all Hardware.
+            TabTipAutomation.IgnoreHardwareKeyboard = HardwareKeyboardIgnoreOptions.IgnoreAll;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pnlcontent.Content = new UniServe.View.Login();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ UniServe/View/POS.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UniServe.View
+{
+    /// <summary>
+    /// Interaction logic for POS.xaml
+    /// </summary>
+    public partial class POS : UserControl
+    {
+        public POS()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WpfApplication1.POS.DayOpen obj = new WpfApplication1.POS.DayOpen();
+            obj.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            WpfApplication1.POS.DayClose obj = new WpfApplication1.POS.DayClose();
+            obj.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            WpfApplication1.POS.OrderBooking obj = new WpfApplication1.POS.OrderBooking();
+            obj.Show();
+        }
+    }
+}
